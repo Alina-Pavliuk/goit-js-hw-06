e4b0a874f50cce68e5536aa6bda4fc2d34e054cb
Alina Pavliuk <alina.alp26@gmail.com>
2020-09-17 17:20:11 +0300

    added method sort

--- task-10.js
+++ task-10.js
@@ -3,12 +3,12 @@ import {default as users} from './users.js'
 const getSortedUniqueSkills = users => {
   let allSkills = [];
   users.forEach(el => allSkills.push(...el.skills));
-  return allSkills.sort().filter((el, i, arr) => arr.indexOf(el) === i);
+  return allSkills.sort().filter((el, i, arr) => arr.indexOf(el) === i).sort();
 };
 // or ->
 // const getSortedUniqueSkills = users => {
-//   const skills = users.reduce((acc, user) => {
-//     return acc.concat(user.skills)
+//   return users.reduce((acc, user) => {
+//     return acc.push(...user.skills)
 //   },[])
 
 //   const set = new Set(skills);
